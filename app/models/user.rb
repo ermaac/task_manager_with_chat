649143e0b2134df8eeed7d7b01789828a59e4e13
Vladislav Ermolovich <vlad.ermaac@gmail.com>
2017-07-26 12:05:31 +0300

    add google authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  PROVIDERS_UID_COLUMN_NAME = {'facebook': 'facebook_id', 'google_oauth2': 'google_id', 'vkontakte': 'vk_id', 'github': 'gh_id'}
+  PROVIDERS_UID_COLUMN_NAME = {:facebook=>'facebook_id', :google_oauth2=>'google_id', :vkontakte=>'vk_id', :github=>'gh_id'}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -32,8 +32,8 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    provider_uid = get_uid(auth.provider)
-    where(provider: auth.provider, facebook_id: auth.uid).first_or_create do |user|
+    provider_uid = get_uid(auth.provider.to_sym)
+    where(provider: auth.provider, provider_uid=>auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       #    user.name = auth.info.name   # assuming the user model has a name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726075952) do
+ActiveRecord::Schema.define(version: 20170726082457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,10 +18,10 @@ ActiveRecord::Schema.define(version: 20170726075952) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "vk_id"
-    t.integer "gh_id"
-    t.integer "facebook_id"
-    t.integer "google_id"
+    t.string "vk_id"
+    t.string "gh_id"
+    t.string "facebook_id"
+    t.string "google_id"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
