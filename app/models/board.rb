c19d8eb46d331e4ac80fa57332973a361c92c7a0
Vladislav Ermolovich <vlad.ermaac@gmail.com>
2017-08-01 14:42:17 +0300

    link lists with boards

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,6 +4,6 @@ class Board < ApplicationRecord
   has_many :lists, dependent: :destroy
   has_many :invitings, dependent: :destroy
   has_one :chat, dependent: :destroy
-  belongs_to :profile
+  belongs_to :user
   validates :title, :description, presence: true
 end
