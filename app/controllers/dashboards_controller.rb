c10a142e4ad838faa2edb109206ea4929fe45235
Vladislav Ermolovich <vlad.ermaac@gmail.com>
2017-08-11 14:48:14 +0300

    improve callback of DashboardsController

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!, only: [:index, :show]
-  before_action :current_user_board, only: :show
+  before_action :find_board, only: :show
 
   def index
     @board = Board.new
@@ -8,17 +8,7 @@ class DashboardsController < ApplicationController
     @number = Invitation.where(user_to_invite_id: current_user.id).count
   end
 
-  def current_user_board
-    user_boards = current_user.boards
-    if !user_boards.ids.include?(params[:id].chomp.to_i)
-      flash[:warning] = "you don't have accepted to this board"
-      redirect_to root_path
-    end
-  end
-
   def show
-    session[:board_id] = params[:id]
-    @board = Board.find params[:id]#fix
     @lists = @board.lists.includes(:notes).order('created_at asc')
     @list = List.new
     @note = Note.new
@@ -42,5 +32,12 @@ class DashboardsController < ApplicationController
     board_id = params[:board_id]
     Invitation.where(board_id: board_id, user_to_invite_id: current_user.id).first.destroy
     {user_id: current_user.id, board_id: board_id}
+  end
+
+  def find_board
+    unless @board = current_user.boards.find_by(id: params[:id])
+      flash[:warning] = "You are not accepted in this board"
+      redirect_to root_path
+    end
   end
 end
