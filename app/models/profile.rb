5950ab834d5a24d8f232fe3719ca927dd0f8e2ce
Dmitry <basher9696@gmail.com>
2017-08-16 14:22:23 +0300

    Gem for images was changed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,9 @@
 class Profile < ApplicationRecord
-  #has_many :boards, dependent: :destroy
-  #has_many :messages, dependent: :destroy
+  has_many :boards, dependent: :destroy
+  has_many :messages, dependent: :destroy
   belongs_to :user
   validates :first_name, :last_name, :birthdate, :sex, presence: true, on: :update
-  mount_uploader :photo_url, PhotoUploader
+  has_attached_file :photo_url, styles: { medium: "300x300>", thumb: "200x200>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :photo_url, content_type: /\Aimage\/.*\z/
   enum sex: [:male, :female]
 end
