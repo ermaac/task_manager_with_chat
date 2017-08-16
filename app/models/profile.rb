ca53edc360cc5662dbb8cfd92227c3c88c67f635
Batracov Dmitrii <basher9696@gmail.com>
2017-08-16 09:09:15 +0300

    Update profile.rb

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,6 @@
 class Profile < ApplicationRecord
-  #has_many :boards, dependent: :destroy
-  #has_many :messages, dependent: :destroy
+  has_many :boards, dependent: :destroy
+  has_many :messages, dependent: :destroy
   belongs_to :user
   validates :first_name, :last_name, :birthdate, :sex, presence: true, on: :update
   mount_uploader :photo_url, PhotoUploader
