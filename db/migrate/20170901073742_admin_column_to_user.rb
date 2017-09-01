5d03ca117188b025acf43a9055677fedee355539
WhipFalcon <tripletwo567@gmail.com>
2017-09-01 11:34:59 +0300

    Delete admin table

--- db/migrate/20170901073742_admin_column_to_user.rb
+++ db/migrate/20170901073742_admin_column_to_user.rb
@@ -1,4 +1,4 @@
-class AddAdminColumnToUser < ActiveRecord::Migration[5.1]
+class AdminColumnToUser < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :admin, :boolean, column_options: { null: false }
   end
